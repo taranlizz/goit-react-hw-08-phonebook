1e861b251a2c0e771a95de45e6d5a195e2e18265
Elizaveta Taran <lizataran103@gmail.com>
2023-11-10 00:07:14 +0200

    linter adjustment

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,7 +2,6 @@ import { ContactList } from 'components/ContactList/ContactList';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/contacts/operations';
-import { Box } from '@mui/material';
 import { ContactBar } from 'components/ContactsBar/ContactsBar';
 import { PageTitle } from 'components/PageTitle/PageTitle';
 
